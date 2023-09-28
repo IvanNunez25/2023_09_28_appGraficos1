612ef44100029f7459aa6a44bfc884b00ef1cf00
IvanNunez25 <ivan25606@gmail.com>
2023-09-28 11:14:30 -0600

    Base del Proyecto

---
+++ 2023_09_30_appGraficos1/Vista1.h
@@ -0,0 +1,16 @@
+//
+//  Vista1.h
+//  2023_09_30_appGraficos1
+//
+//  Created by Ivanovicx Nuñez on 28/09/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Vista1 : NSView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2023_09_30_appGraficos1/Vista1.m
@@ -0,0 +1,19 @@
+//
+//  Vista1.m
+//  2023_09_30_appGraficos1
+//
+//  Created by Ivanovicx Nuñez on 28/09/23.
+//
+
+#import "Vista1.h"
+
+@implementation Vista1
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+    
+}
+
+@end
